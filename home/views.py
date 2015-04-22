2aeb4d6c537c7098a9c292fdcb19e959b892171b
fast4shoot <z.biberle@gmail.com>
2015-04-22 13:25:46 +0200

    Index, urls a templaty

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
 
-# Create your views here.
+def index(request):
+	video_list = models.Video.objects.order_by("-upvotes")[:10]
+	context = {
+		"video_list" : video_list,
+	}
+	return render(request, "home/index.html", context)
