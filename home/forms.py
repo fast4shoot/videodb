28b9ec339c4ce0769a137619b61bbf84b5ed2a40
Jozkar <j.ridky@gmail.com>
2015-05-12 17:14:01 +0200

    stylovani

--- home/forms.py
+++ home/forms.py
@@ -7,9 +7,9 @@ class SearchForm(forms.Form):
 	term = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Zadejte hledaný výraz', 'size': '30'}))
 
 class UploadForm(forms.Form):
-	name = forms.CharField(max_length = 256, label='Název videa')
+	name = forms.CharField(max_length = 256, label='Název videa',widget=forms.TextInput(attrs={'size': '60'}))
 	description = forms.CharField(widget=forms.Textarea(attrs={'rows': '6', 'cols': '60'}), label='Popis videa')
-	tags = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'jednotlivé tagy oddělujte mezerou', 'size': '30'}), label='Tagy')
+	tags = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'jednotlivé tagy oddělujte mezerou', 'size': '60'}), label='Tagy')
 	file = forms.FileField(label='Video')
 
 class CommentForm(forms.Form):
@@ -18,8 +18,8 @@ class CommentForm(forms.Form):
 class RegisterForm(forms.Form):
 	username = forms.CharField(max_length = 30, min_length = 1, label = 'Uživatelské jméno')
 	email = forms.EmailField(label = 'E-mail')
-	password = forms.CharField(label = '')
+	password = forms.CharField(widget=forms.PasswordInput, label = 'Heslo')
 
 class VideoEditForm(forms.Form):
-	name = forms.CharField(max_length = 256, label='Název videa')
-	description = forms.CharField(widget=forms.Textarea(attrs={'rows': '6', 'cols': '60'}), label='Popis videa')
+	name = forms.CharField(widget=forms.TextInput(attrs={'class': 'hinput'}), max_length = 256, label='Název videa')
+	description = forms.CharField(widget=forms.Textarea(attrs={'rows': '6', 'class': 'ctxtarea'}), label='Popis videa')
