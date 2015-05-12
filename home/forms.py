bf5ec5a6233c50672bff1d0f47b8e1787697f289
Jozkar <j.ridky@gmail.com>
2015-05-12 14:35:52 +0200

    forms editing

--- home/forms.py
+++ home/forms.py
@@ -5,13 +5,13 @@ class SearchForm(forms.Form):
 	term = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Zadejte hledaný výraz', 'size': '30'}))
 
 class UploadForm(forms.Form):
-	name = forms.CharField(max_length = 256)
-	description = forms.CharField()
-	tags = forms.CharField()
-	file = forms.FileField()
+	name = forms.CharField(max_length = 256, label='Název videa')
+	description = forms.CharField(widget=forms.Textarea(attrs={'rows': '6', 'cols': '100'}, label='Popis videa')
+	tags = forms.CharField(label='Tagy (více oddělujte mezerou)')
+	file = forms.FileField(label='Video')
 
 class CommentForm(forms.Form):
-	comment = forms.CharField(min_length = 10)
+	comment = forms.CharField(widget=forms.Textarea(attrs={'rows': '4', 'cols': '100'}, min_length = 10)
 
 class RegisterForm(forms.Form):
 	username = forms.CharField(max_length = 30, min_length = 1)
